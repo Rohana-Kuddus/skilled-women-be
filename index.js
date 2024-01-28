1583f43351ee0372c0f562390ef52da54e64a439
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-28 10:33:29 +0700

    merapihkan kode index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
 const express = require('express');
 const cityRoutes = require('./routes/city')
+const app = express();
+require('dotenv').config();
 
-const app = express(); // membuat express App
-
-app.use(express.json()); // menerima jsond dari body disaat POST request
-app.use(cityRoutes); // ambil  routes city
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
- console.log(`Running on port ${process.env.PORT}`);
+  console.log(`Running on port ${process.env.PORT}`);
 });
+
+//route
+app.use(cityRoutes);
