995a4314df5bab7b8565d5d9eeee45b9a86872d7
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-28 10:21:05 +0700

    menambahkan city controller, route dan memanggilnya di index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
+const express = require('express');
+const cityRoutes = require('./routes/city')
+
+const app = express(); // membuat express App
+
+app.use(express.json()); // menerima jsond dari body disaat POST request
+app.use(cityRoutes); // ambil  routes city
 
 app.listen(process.env.PORT, () => {
  console.log(`Running on port ${process.env.PORT}`);
---
+++ routes/city.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const route = express.Router();
+
+const {
+  getAllCity, 
+  getCityById
+} = require('../controllers/city'); // import tea controller
+// const verifyToken = require("../middlewares/auth")
+
+
+//endpoints:
+route.get('/cities', getAllCity); 
+route.get('/:id', getCityById); 
+
+// Jika pakai token
+// route.get('/cities', verifyToken, getAllCity); 
+// route.get('/:id', verifyToken, getCityById); 
+
+module.exports = route;
