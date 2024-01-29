5ace7bb44e9807d3393eac5fcf6a9252f073beed
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-29 14:06:28 +0700

    perbaikan kode

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
+
 const { auth } = require('./routes/auth');
 const { industry } = require('./routes/industry');
-const userRoutes = require('./routes/user');
 const app = express();
 require('dotenv').config();
-
+const userRoutes = require('./routes/user');
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
  console.log(`Running on port ${process.env.PORT}`);
 });
 
+
+
 //routes
+
 app.use(auth);
 app.use(industry);
-
 app.use(userRoutes);
