08a1dea049ce429cb424435be01755f4185568f6
syahba <ksyaahba@gmail.com>
2024-02-07 12:45:07 +0700

    create api reset password and check user

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,13 @@ const express = require('express');
 const {
   getUserProfile,
   updateUserProfile,
-  updateUserPassword,
-  checkUser,
+  updateUserPassword
 } = require("../controllers/user");
 const { verifyToken } = require('../middlewares/auth');
 const user = express.Router();
 
 user.get('/users', verifyToken, getUserProfile);
 user.put('/users', verifyToken, updateUserProfile);
-user.patch('/users', updateUserPassword);
-user.post('/users', checkUser);
+user.patch('/users', verifyToken, updateUserPassword);
 
 module.exports = user;
